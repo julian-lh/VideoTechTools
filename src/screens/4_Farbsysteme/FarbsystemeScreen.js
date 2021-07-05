769a42f1b26b60af1c969b324b1d3947d855eb58
Julio <j.lopesh@yahoo.com>
2021-07-05 09:03:53 +0200

    Upgrade SDK 42, Packages react-three-fiber, CIE-View eingebaut

--- src/screens/4_Farbsysteme/FarbsystemeScreen.js
+++ src/screens/4_Farbsysteme/FarbsystemeScreen.js
@@ -1,11 +1,88 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Header, Text, Slider } from 'react-native-elements';
+import CIEView from '../../ThreeJSViews/Scopes/CIEView';
 
+import { rgbToXYZ } from '../../calculation/ColorSpaceTransform';
+
+const ColorSelector = (props) => {
+    return (
+        <View style={styles.inputElement}>
+            <Text style={styles.inputElementLabel}>{props.label}</Text>
+            <Button style={styles.inputElementButtons} title='<' onPress={() => props.valueChange(props.value - 0.1)}/>
+            <Slider style={styles.inputElementSlider}
+            value={props.value}
+            onValueChange={(x) => props.valueChange(x)}
+            minimumTrackTintColor={props.trackColor}
+            thumbTintColor={props.thumbTintColor}
+            thumbStyle={styles.inputElementSliderThumb}
+            />
+            <Button style={styles.inputElementButtons} title='>' onPress={() => props.valueChange(props.value + 0.1)}/>
+        </View>
+    );
+}
 
 export default function FarbsystemeScreen() {
+    const [red, setRed] = useState(0.5);
+    const [green, setGreen] = useState(0.5);
+    const [blue, setBlue] = useState(0.5);
+    const [colorSpaceIndex, setColorSpaceIndex] = useState(0);
+    const colorSpaces = ['sRGB', 'Adobe RGB', 'rec709', 'rec2020'];
+
+    const XYZ = rgbToXYZ([red, green, blue]);
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Farbsysteme</Text>
+      <View style={{ flex: 1}}>
+        <CIEView XYZ={XYZ} />
+
+        <View style={styles.colorPicker}>
+            <ColorSelector label='R' thumbTintColor='red' value={red} valueChange={(x) => setRed(x)}/>
+            <ColorSelector label='G' thumbTintColor='green'  value={green} valueChange={(x) => setGreen(x)}/>
+            <ColorSelector label='B' thumbTintColor='blue' value={blue} valueChange={(x) => setBlue(x)}/>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', backgroundColor: '#ccc', paddingVertical: 5}}>
+            <Button title='RGB/HSV'/>
+            <Button title='FARBRAUM'/>
+        </View>
+
       </View>
     );
   }
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    colorPicker: {
+        flex: 1,
+        backgroundColor: '#ddd',
+        maxHeight: 150
+    },
+
+
+    inputElement: {
+        flex: 1,
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: 'center',
+        paddingHorizontal: 20
+      },
+      inputElementLabel: {
+        fontSize: 20,
+      },
+      inputElementButtons: {
+        padding: 5,
+        width: 40
+      },
+      inputElementSlider:{
+        flex: 1,
+      },
+      inputElementSliderThumb:{
+        width: 20,
+        height: 20
+      },
+  });
