2444e73f27ed9941fe416b58613cb8d6f9f85d6c
Julio <j.lopesh@yahoo.com>
2021-07-27 23:02:20 +0200

    Generator etwas schlanker, allerdings etwas langsamer geworden

--- src/components/signalGenerator/SignalGenerator.js
+++ src/components/signalGenerator/SignalGenerator.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
 import { Button, Text, Slider, ButtonGroup } from 'react-native-elements';
 
@@ -6,26 +6,17 @@ import { styles } from './SignalGeneratorStyle';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import { TapButton, BtnGroup } from './Subviews/TapButton';
-import { FullColorSelector, GradientSelector, BarsSelector } from './Subviews/SelectorViews'
+import { TapButton } from './Subviews/TapButton';
+import { FullColorGenerator, GradientGenerator, BarsGenerator} from './Subviews/SelectorViews'
 
-import { cvtRGBtoHSV, cvtHSVtoRGB } from '../../calculations/ColorSpaceTransform';
 import { cvtSignalRGBtoYCRCB, upscaleSignalYCRCB, limiterComponentSignal, limiterRGBSignal} from '../../calculations/ComponentSignal';
-import { generateRGBSignalFullColor, generateRGBSignalGradient, generateRGBSignalBars, offsetSignalContrast, offsetSignalBrightness, offsetSignalGamma, generateRGB3dCoordinates } from '../../calculations/SignalGenerator';
-
-import { clamp } from '../../calculations/Helpers';
+import { offsetSignalContrast, offsetSignalBrightness, offsetSignalGamma } from '../../calculations/SignalGenerator';
 
 
-const Generator = ({generatorIdx, setGeneratorIdx,
-                    red, setRed, green, setGreen, blue, setBlue,
-                    hue, setHue, sat, setSat, val, setVal,
-                    fStopOffset, setFStopOffset,
-                    showRGB, toggleRgbHsv,
-                    gradientColor1, setGradientColor1,
-                    gradientColor2, setGradientColor2,
-                    directionHorizontal, toggleDirection,
-                    useBars100, setUseBars100}) => {
 
+const Generator = ({ setRgbSignal, fStopOffset, setFStopOffset }) => {
+
+    const [generatorIdx, setGeneratorIdx] = useState(0);
 
     return(
         <View style={{ flex: 1, justifyContent: 'center', alignItems: "center" }}>
@@ -35,24 +26,11 @@ const Generator = ({generatorIdx, setGeneratorIdx,
                 <Button title="Bars" onPress={()=>setGeneratorIdx(2)} titleStyle={{ color: (generatorIdx == 2 ? "black" : "gray")}} type="clear"/>
             </View>
 
+            {generatorIdx === 0 ? <FullColorGenerator setRgbSignal={setRgbSignal} /> : null}
 
-            {generatorIdx === 0 ?
-            <FullColorSelector red={red} setRed={setRed}
-                        green={green} setGreen={setGreen}
-                        blue={blue} setBlue={setBlue}
-                        hue={hue} setHue={setHue}
-                        sat={sat} setSat={setSat}
-                        val={val} setVal={setVal}
-                        showRGB={showRGB} toggleRgbHsv={toggleRgbHsv}
-            /> : null}
+            {generatorIdx === 1 ? <GradientGenerator setRgbSignal={setRgbSignal} /> : null}
 
-            {generatorIdx === 1 ?
-                <GradientSelector rgb1={gradientColor1} setRGB1={setGradientColor1}  rgb2={gradientColor2} setRGB2={setGradientColor2} directionHorizontal={directionHorizontal} toggleDirection={toggleDirection} />
-            : null}
-
-            {generatorIdx === 2 ?
-                <BarsSelector useBars100={useBars100} toggleBarsType={() => setUseBars100(!useBars100)}/>
-            : null}
+            {generatorIdx === 2 ? <BarsGenerator setRgbSignal={setRgbSignal} /> : null}
 
             <TapButton label={"Blenden Offset"} currentValue={fStopOffset} setValue={setFStopOffset} stepSize={0.05}/>
 
@@ -82,6 +60,9 @@ const Corrector = ({contrastOffset, setContrastOffset, gammaOffset, setGammaOffs
 
  export const YCrCbGenerator = ({setSignal, setEncodingVideoStandard, showHideButton = false}) => {
 
+    const [hideGeneratorView, setHideGeneratorView] = useState(false);
+    const [pageID, setPageID] = useState(0);
+
     const videoStandards = ["601", "709", "2020"];
     const [vidStdIdx, setVidStdIdx] = useState(1);
     const switchVidStd = () => {vidStdIdx < 2 ? setVidStdIdx(vidStdIdx + 1) : setVidStdIdx(0)};
@@ -90,92 +71,30 @@ const Corrector = ({contrastOffset, setContrastOffset, gammaOffset, setGammaOffs
     const [bitDepthIdx, setBitDepthIdx] = useState(0);
     const switchBitDepth = () => setBitDepthIdx(1 - bitDepthIdx);
 
-    // Überpegel erlauben
     const [exceedVideoLevels, setExceedVideoLevels] = useState(false);
 
-    const [hideGeneratorView, setHideGeneratorView] = useState(false);
-    const [pageID, setPageID] = useState(0);
-    const [generatorIdx, setGeneratorIdx] = useState(0);
-
-
-    // COLOR-PICKER SINGLE COLOR
-    const [hue, setHueDirectly] = useState(0);
-    const setHue = (value)  => setHueDirectly((value % 360 < 0) ? (360 - Math.abs(value) % 360) : value % 360);
-    const [saturation, setSaturationDirectly] = useState(0);
-    const setSaturation = (value)  => setSaturationDirectly( clamp(value) );
-    const [value, setValueDirectly] = useState(0);
-    const setValue = (value)  => setValueDirectly( clamp(value) );
-
-    const [red, setRedDirectly] = useState(0.0);
-    const setRed = (value)  => setRedDirectly( clamp(value) );
-    const [green, setGreenDirectly] = useState(0.0);
-    const setGreen = (value)  => setGreenDirectly( clamp(value) );
-    const [blue, setBlueDirectly] = useState(0.0);
-    const setBlue = (value)  => setBlueDirectly( clamp(value) );
-
-    // prevent infinite conversion loops
-    const [showingRgbControls, setShowingRgbControls] = useState(true);
-
-    useEffect(() => {
-        if (!showingRgbControls){
-            const [r, g, b] = cvtHSVtoRGB([hue, saturation, value]);
-            setRedDirectly(r);
-            setGreenDirectly(g);
-            setBlueDirectly(b);
-        }
-    }, [hue, saturation, value]);
-
-    useEffect(() => {
-        if (showingRgbControls){
-            const [h, s, v] = cvtRGBtoHSV([red, green, blue]);
-            setHueDirectly(h);
-            setSaturationDirectly(s);
-            setValueDirectly(v);
-        }
-    }, [red, green, blue]);
-
-
-    // GRADIENT COLORS
-    const [gradientColor1, setGradientColor1] = useState([1, 0, 1]);
-    const [gradientColor2, setGradientColor2] = useState([0, 1, 0]);
-    const [horizontalGradientDirection, setHorizontalGradientDirection] = useState(true);
-    var amountGradientPixelH =  (horizontalGradientDirection ? 8 : 1);
-    var amountGradientPixelV =  (horizontalGradientDirection ? 1 : 8);
-
-    const [useBars100, setUseBars100] = useState(true);
-
-    var signalRGB = [[[0, 0, 0]]];
-
-    switch(generatorIdx) {
-        case 0:
-            signalRGB = generateRGBSignalFullColor([red, green, blue], 1, 1);
-            break;
-        case 1:
-            signalRGB = generateRGBSignalGradient(gradientColor1, gradientColor2, amountGradientPixelH, amountGradientPixelV, horizontalGradientDirection);
-            break;
-        case 2:
-            signalRGB = generateRGBSignalBars(8, 1, useBars100); //generateRGB3dCoordinates(); //
-            break;
-    }
+    const [signalRGB, setSignalRGB] = useState( [[[0, 0, 0]]] );
 
     const [fStopOffset, setFStopOffset] = useState(0); //[0...2]
-
     const [contrastOffset, setContrastOffset] = useState(1); //[0...2]
     const [brightnessOffset, setBrightnessOffset] = useState(0); //[-2...2]
     const [gammaOffset, setGammaOffset] = useState(1); //[-3...3]
 
 
     // Modifizieren (Operationen nur anwenden wenn nicht neutral)
-    signalRGB = (fStopOffset != 0 ? offsetSignalBrightness(signalRGB, fStopOffset) : signalRGB);
+    const postCorrectorSignal = useMemo(() => {
+        var signal = signalRGB;
+        signal = (fStopOffset != 0 ? offsetSignalBrightness(signal, fStopOffset) : signal);
+        signal = (contrastOffset != 1 ? offsetSignalContrast(signal, contrastOffset) : signal);
+        signal = (brightnessOffset != 0 ? offsetSignalBrightness(signal, brightnessOffset) : signal);
+        signal = (gammaOffset != 1 ? offsetSignalGamma(signal, gammaOffset) : signal);
+        signal = (exceedVideoLevels ?  signal : limiterRGBSignal(signal));
+        return signal;
+    }, [fStopOffset, contrastOffset, gammaOffset, brightnessOffset, exceedVideoLevels, bitDepthIdx, vidStdIdx, signalRGB])
 
-    signalRGB = (contrastOffset != 1 ? offsetSignalContrast(signalRGB, contrastOffset) : signalRGB);
-    signalRGB = (brightnessOffset != 0 ? offsetSignalBrightness(signalRGB, brightnessOffset) : signalRGB);
-    signalRGB = (gammaOffset != 1 ? offsetSignalGamma(signalRGB, gammaOffset) : signalRGB);
-
-    signalRGB = (exceedVideoLevels ?  signalRGB : limiterRGBSignal(signalRGB));
 
     // RGB -> YCrCb
-    const signalSmallYCRCB = cvtSignalRGBtoYCRCB(signalRGB, videoStandards[vidStdIdx]);
+    const signalSmallYCRCB = cvtSignalRGBtoYCRCB(postCorrectorSignal, videoStandards[vidStdIdx]);
     var signalYCRCB = upscaleSignalYCRCB(signalSmallYCRCB, bitDepths[bitDepthIdx]);
     signalYCRCB = limiterComponentSignal(signalYCRCB, bitDepths[bitDepthIdx], exceedVideoLevels);
 
@@ -183,12 +102,13 @@ const Corrector = ({contrastOffset, setContrastOffset, gammaOffset, setGammaOffs
     // Trigger recalculation of signal when values change
     useEffect(() => {
         setSignal(signalYCRCB);
-   }, [fStopOffset, red, green, blue, gradientColor1, gradientColor2, horizontalGradientDirection, useBars100, generatorIdx]);
+        setEncodingVideoStandard(vidStdIdx);
+   }, [signalRGB]);
 
     useEffect(() => {
         setSignal(signalYCRCB);
         setEncodingVideoStandard(vidStdIdx);
-    },[contrastOffset, gammaOffset, brightnessOffset, generatorIdx, exceedVideoLevels, bitDepthIdx, vidStdIdx]);
+    },[fStopOffset, contrastOffset, gammaOffset, brightnessOffset, exceedVideoLevels, bitDepthIdx, vidStdIdx]);
 
 
     return (
@@ -201,20 +121,7 @@ const Corrector = ({contrastOffset, setContrastOffset, gammaOffset, setGammaOffs
         <ScrollView width="100%" contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: "center"}} style={{ backgroundColor: "#dedede" }}>
 
             {pageID == 0 ?
-            <Generator generatorIdx={generatorIdx} setGeneratorIdx={setGeneratorIdx}
-                        red={red} setRed={setRed}
-                        green={green} setGreen={setGreen}
-                        blue={blue} setBlue={setBlue}
-                        hue={hue} setHue={setHue}
-                        sat={saturation} setSat={setSaturation}
-                        val={value} setVal={setValue}
-                        fStopOffset={fStopOffset} setFStopOffset={setFStopOffset}
-                        showRGB={showingRgbControls} toggleRgbHsv={() => setShowingRgbControls(!showingRgbControls)}
-                        gradientColor1={gradientColor1} setGradientColor1={setGradientColor1}
-                        gradientColor2={gradientColor2} setGradientColor2={setGradientColor2}
-                        directionHorizontal={horizontalGradientDirection} toggleDirection={() => setHorizontalGradientDirection(!horizontalGradientDirection)}
-                        useBars100={useBars100} setUseBars100={setUseBars100}
-            />
+            <Generator rgbSignal={signalRGB} setRgbSignal={setSignalRGB} fStopOffset={fStopOffset} setFStopOffset={setFStopOffset} />
             :
             <Corrector contrastOffset={contrastOffset} setContrastOffset={setContrastOffset}
                         gammaOffset={gammaOffset} setGammaOffset={setGammaOffset}
--- src/components/signalGenerator/Subviews/SelectorViews.js
+++ src/components/signalGenerator/Subviews/SelectorViews.js
@@ -1,18 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native';
 import { Button, Text } from 'react-native-elements';
-
 import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { cvtRGBtoHSV, cvtHSVtoRGB } from '../../../calculations/ColorSpaceTransform';
+import { generateRGBSignalFullColor, generateRGBSignalGradient, generateRGBSignalBars } from '../../../calculations/SignalGenerator';
+
+import { clamp } from '../../../calculations/Helpers';
 
 import { TapButton } from './TapButton';
 
 
 
-export const FullColorSelector = ({red, setRed, green, setGreen, blue, setBlue, hue, setHue, sat, setSat, val, setVal, showRGB, toggleRgbHsv}) => {
+export const FullColorGenerator = ({ setRgbSignal }) => {
+
+    const [hue, setHueDirectly] = useState(0);
+    const setHue = (value)  => setHueDirectly((value % 360 < 0) ? (360 - Math.abs(value) % 360) : value % 360);
+    const [saturation, setSaturationDirectly] = useState(0);
+    const setSaturation = (value)  => setSaturationDirectly( clamp(value) );
+    const [value, setValueDirectly] = useState(0);
+    const setValue = (value)  => setValueDirectly( clamp(value) );
+
+    const [red, setRedDirectly] = useState(0.0);
+    const setRed = (value)  => setRedDirectly( clamp(value) );
+    const [green, setGreenDirectly] = useState(0.0);
+    const setGreen = (value)  => setGreenDirectly( clamp(value) );
+    const [blue, setBlueDirectly] = useState(0.0);
+    const setBlue = (value)  => setBlueDirectly( clamp(value) );
+
+     // prevent infinite conversion loops
+    const [showingRgbControls, setShowingRgbControls] = useState(true);
+
+    useEffect(() => {
+        if (!showingRgbControls){
+            const [r, g, b] = cvtHSVtoRGB([hue, saturation, value]);
+            setRedDirectly(r);
+            setGreenDirectly(g);
+            setBlueDirectly(b);
+        }
+    }, [hue, saturation, value]);
+
+    useEffect(() => {
+        if (showingRgbControls){
+            const [h, s, v] = cvtRGBtoHSV([red, green, blue]);
+            setHueDirectly(h);
+            setSaturationDirectly(s);
+            setValueDirectly(v);
+        }
+    }, [red, green, blue]);
+
+    useEffect(() => {
+    setRgbSignal(generateRGBSignalFullColor([red, green, blue], 1, 1));
+    }, [red, green, blue])
+
     return(
         <View style={{ flex: 1, flexGrow: 1, justifyContent: 'center', alignItems: "center",}}>
-            <Button title={(showRGB ? "RGB" : "HSV")} onPress={toggleRgbHsv} style={{ paddingTop: 10  }} titleStyle={{ color: "black"}} type={"outline"}/>
-            {showRGB ?
+            <Button title={(showingRgbControls ? "RGB" : "HSV")} onPress={() => setShowingRgbControls(!showingRgbControls)} style={{ paddingTop: 10  }} titleStyle={{ color: "black"}} type={"outline"}/>
+            {showingRgbControls ?
                 <View style={{ flex: 1,  flexDirection: "column", justifyContent: 'space-around', alignItems: "center", padding: 10 }}>
                     <TapButton label={"Rot"} currentValue={red} setValue={setRed} stepSize={0.1} color={"#fdd"} />
                     <TapButton label={"Grün"} currentValue={green} setValue={setGreen} stepSize={0.1} color={"#dfd"}/>
@@ -20,8 +64,8 @@ export const FullColorSelector = ({red, setRed, green, setGreen, blue, setBlue, 
                 </View> :
                 <View style={{ flex: 1, flexDirection: "column", justifyContent: 'space-around', alignItems: "center", padding: 10}}>
                     <TapButton label={"Hue"} currentValue={hue} setValue={setHue} stepSize={5} stepSize2={20}/>
-                    <TapButton label={"Saturation"} currentValue={sat} setValue={setSat} stepSize={0.1}/>
-                    <TapButton label={"Value"} currentValue={val} setValue={setVal} stepSize={0.1}/>
+                    <TapButton label={"Saturation"} currentValue={saturation} setValue={setSaturation} stepSize={0.1}/>
+                    <TapButton label={"Value"} currentValue={value} setValue={setValue} stepSize={0.1}/>
                 </View>}
 
         </View>
@@ -29,20 +73,30 @@ export const FullColorSelector = ({red, setRed, green, setGreen, blue, setBlue, 
 }
 
 
-export const GradientSelector = ({rgb1, setRGB1, rgb2, setRGB2, directionHorizontal, toggleDirection}) => {
+export const GradientGenerator = ({ setRgbSignal }) => {
+    const [gradientColor1, setGradientColor1] = useState([1, 0, 1]);
+    const [gradientColor2, setGradientColor2] = useState([0, 1, 0]);
+    const [directionHorizontal, setDirectionHorizontal] = useState(true);
+    var amountGradientPixelH =  (directionHorizontal ? 8 : 1);
+    var amountGradientPixelV =  (directionHorizontal ? 1 : 8);
+
+    useEffect(() => {
+        setRgbSignal(() => generateRGBSignalGradient(gradientColor1, gradientColor2, amountGradientPixelH, amountGradientPixelV, directionHorizontal))
+     }, [gradientColor1, gradientColor2, directionHorizontal])
+
     return(
         <View>
-            <Button title={(directionHorizontal ? "horizontal" : "vertikal")} onPress={toggleDirection}  titleStyle={{ color: "black"}} containerStyle={{ padding: 5 }} type={"outline"}/>
+            <Button title={(directionHorizontal ? "horizontal" : "vertikal")} onPress={ () => setDirectionHorizontal(!directionHorizontal)}  titleStyle={{ color: "black"}} containerStyle={{ padding: 5 }} type={"outline"}/>
             <View style={{  flexDirection: "row", justifyContent: 'space-around', alignItems: "center" }}>
-            <ColorPad setColor={setRGB1}/>
+            <ColorPad setColor={setGradientColor1}/>
             <Ionicons name={"arrow-forward"} size={30} color={"gray"} />
-            <ColorPad setColor={setRGB2}/>
+            <ColorPad setColor={setGradientColor2}/>
             </View>
         </View>
     )
 }
 
-const ColorPad = ({setColor}) => {
+const ColorPad = ({ setColor }) => {
     //const gradientColors = [[1, 0, 0 ], [1, 1, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1], [1, 0, 1], [0, 0, 0], [1, 1, 1]];
     const buttonSize = 40;
     return(
@@ -66,8 +120,14 @@ const ColorPad = ({setColor}) => {
 
 
 
-export const BarsSelector = ({ useBars100, toggleBarsType }) => {
+export const BarsGenerator = ({ setRgbSignal }) => {
+    const [useBars100, setUseBars100] = useState(true);
+
+    useEffect(() => {
+        setRgbSignal(() => generateRGBSignalBars(8, 1, useBars100))
+     }, [useBars100])
+
     return(
-        <Button title={(useBars100 ? "Typ 100/100" : "Typ 100/75")} onPress={toggleBarsType} style={{ padding: 10, width: 200 }} titleStyle={{ color: "black"}} type={"outline"} />
+        <Button title={(useBars100 ? "Typ 100/100" : "Typ 100/75")} onPress={() => setUseBars100(!useBars100)} style={{ padding: 10, width: 200 }} titleStyle={{ color: "black"}} type={"outline"} />
         )
 }
